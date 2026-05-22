461e78b2ab310b6aa2e4736daefc2af465e205ab
gerzacruzajamera <gerzajamera07@gmail.com>
2026-05-22 08:31:06 +0800

    Resolve assembly info duplication and configuration conflict

--- Applicant Tracking/Properties/AssemblyInfo.cs
+++ Applicant Tracking/Properties/AssemblyInfo.cs
@@ -2,20 +2,14 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ApplicantTrackingSystem")]
-[assembly: AssemblyDescription("A desktop-based Applicant Tracking System for job management and recruitment workflow.")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("ATS Desktop Application")]
 [assembly: AssemblyCompany("CareerLink Systems")]
 [assembly: AssemblyProduct("CareerLink ATS")]
 [assembly: AssemblyCopyright("Copyright © 2026")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// COM visibility disabled for security
 [assembly: ComVisible(false)]
 
-// COM GUID (unchanged)
 [assembly: Guid("02cc3f19-d38d-4637-9a46-2863ea4fbece")]
 
-// Versioning
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
